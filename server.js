fabb5b82d8d467cd84e0566ab5ff9ff3763c7bf0
msselizabeth <lizagotovitryby@gmail.com>
2023-05-11 16:49:33 +0300

    connect mongoose

--- server.js
+++ server.js
@@ -1,5 +1,22 @@
 const app = require('./app')
+const mongoose = require("mongoose");
 
-app.listen(3000, () => {
+const DB_HOST = "mongodb+srv://msselizabeth:KeUczi4rlvMIbxhz@cluster0.govejgr.mongodb.net/db-contacts?retryWrites=true&w=majority";
+  
+mongoose.set('strictQuery', true);
+
+mongoose.connect(DB_HOST)
+  .then(() => {
+    console.log("Database connection successful");
+    app.listen(3000, () => {
   console.log("Server running. Use our API on port: 3000")
-})
+}) 
+  })
+  .catch(error => {
+  console.log(error.message);
+  process.exit(1);
+  })
+
+
+// 
+
