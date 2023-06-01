d23e71186fd28a58ad0b0b2f32f0ca4ab41502c9
msselizabeth <lizagotovitryby@gmail.com>
2023-06-01 15:53:51 +0300

    hw-05 done

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const bcrypt = require('bcrypt');
+const gravatar = require('gravatar');
+const path = require('path');
+const fs = require('fs/promises');
+const Jimp = require('jimp');
 
 const router = express.Router();
-const { authenticate } = require('../../middlewares');
+const { authenticate, upload } = require('../../middlewares');
 
 const { schemas, User } = require('../../models/user');
 const { HttpError } = require('../../helpers');
@@ -10,6 +14,8 @@ const { HttpError } = require('../../helpers');
 const jwt = require('jsonwebtoken');
 
 const { SECRET_KEY } = process.env;
+
+const avatarsDir = path.join(__dirname, '../../', 'public', 'avatars');
 
 router.post('/register', async (req, res, next) => {
     try {
@@ -24,9 +30,10 @@ router.post('/register', async (req, res, next) => {
             throw HttpError(409, 'Email in use');
         }
 
-        const hashPassword = await bcrypt.hash(password, 10); //створюємо хеш
+        const hashPassword = await bcrypt.hash(password, 10);
+        const avatarURL = gravatar.url(email); 
         
-        const newUser = await User.create({...req.body, password: hashPassword}); //змінюємо пароль на хеш
+        const newUser = await User.create({...req.body, password: hashPassword, avatarURL}); 
         res.status(201).json({      
             email: newUser.email,        
             name: newUser.name,
@@ -94,5 +101,31 @@ router.post('/logout', authenticate, async (req, res, next) => {
         next(error);
     }
 })
+
+router.patch('/avatars', authenticate, upload.single('avatar'), async (req, res, next) => {
+    try {
+        const { _id } = req.user;
+        const { path: tempUpload, originalname } = req.file;
+
+        const filename = `${_id}_${originalname}`;
+        const resultUpload = path.join(avatarsDir, filename);
+        
+
+        const ava = await Jimp.read(tempUpload);
+        ava.resize(250, 250).writeAsync(tempUpload);
+
+        await fs.rename(tempUpload, resultUpload);
+
+        const avatarURL = path.join('avatars', filename);
+        await User.findByIdAndUpdate(_id, { avatarURL });
+
+        res.status(200).json({
+            avatarURL,
+        });
+    }
+    catch(error) {
+        next(error);
+    } 
+} )
 
 module.exports = router; 
